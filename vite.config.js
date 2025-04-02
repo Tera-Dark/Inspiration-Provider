67f2e6271e2cd58aa7d70de0bdb516ffc33872eb
Tera-Dark <wjx19825323597@gmail.com>
2025-04-02 22:08:25 +0800

    更新vite配置以支持GitHub Pages部署

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/Inspiration-Provider/',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
