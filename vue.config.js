a22cdf8096a6c3d311e9f77d30120df908816896
Tera-Dark <wjx19825323597@gmail.com>
2025-04-04 21:06:39 +0800

    Updates

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/Inspiration-Provider/'
-    : '/',
-  // 如果需要，可以在这里添加其他配置
-  outputDir: 'dist',
-  assetsDir: '',
-  productionSourceMap: false
-}; 
--- 启动开发服务器.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo 正在启动灵感提供机 Vue版本开发服务器...
-echo.
-
-cd /d "%~dp0"
-npm run dev
-
-pause 
